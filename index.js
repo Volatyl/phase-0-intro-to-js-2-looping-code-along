ad2ef695eae2ff7792ceeba25b20677ecb28b27c
Kennedy Muinde <kennedymuinde@gmail.com>
2023-03-16 21:27:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(arr) {
+  const array = [];
+  for (let i = 0; i < arr.length; i++) {
+    let msg = `Thank you, ${arr[i]}, for the wonderful surprise gift!`;
+    array.push(msg);
+  }
+  return array;
+}
+
+function countDown(num) {
+  let i = 0;
+  while (i <= num) {
+    console.log(i);
+    i++;
+  }
+  return i;
+}
